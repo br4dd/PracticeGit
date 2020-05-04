ff7d3d7ad9bacf25af7d1ce6be6f61a9eeb38237
br4dd <br4dd@gmail.com>
2020-05-04 20:14:11 +0800

    V01

--- sample.py
+++ sample.py
@@ -1,10 +1,14 @@
 def print_name(name):
-    # THIS IS ANOTHER COMMENT!
-    # JUST ADDING A COMMENT!
-
-    print(f'HI THERE, {name.upper()}')
+    hasdigit = False
+    for letter in name:
+        if letter.isdigit():
+            hasdigit = True
+            break
+    if not hasdigit:
+        print(f'HI THERE, {name.upper()}')
+    else:
+        print("NAME MUST NOT CONTAIN DIGIT!")
 
 
 input_name = input("ENTER YOUR NAME: ")
 print_name(input_name)
-print("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.")
